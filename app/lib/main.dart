03e1bf02e8964c9a482ef2909dfb3ecde361a45e
MikeDQG <mike.govedic@gmail.com>
2026-01-31 23:33:24 +0100

    navigation complete

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:app/page1.dart';
 import 'package:app/scanner.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,6 +23,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: Padding(
@@ -31,22 +32,38 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _navButton(
-              context,
-              text: 'Page 1',
-              page: const Placeholder(),
+            CardButton(
+              title: 'Skeniraj crtno kodo',
+              icon: Icons.qr_code_scanner,
+              theme: theme,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PageOne()),
+                );
+              },
             ),
-            const SizedBox(height: 16),
-            _navButton(
-              context,
-              text: 'Page 2',
-              page: const Placeholder(),
+            CardButton(
+              title: 'Isci po imenu',
+              icon: Icons.search,
+              theme: theme,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PageOne()),
+                );
+              },
             ),
-            const SizedBox(height: 16),
-            _navButton(
-              context,
-              text: 'Page 3',
-              page: const Placeholder(),
+            CardButton(
+              title: 'Zgodovina pregledov',
+              icon: Icons.history,
+              theme: theme,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const PageOne()),
+                );
+              },
             ),
           ],
         ),
@@ -72,6 +89,74 @@ class HomePage extends StatelessWidget {
         child: Text(
           text,
           style: const TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
+
+Widget buildCard(IconData icon, String text, ThemeData theme) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+    child: Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.white, size: 50),
+            const SizedBox(width: 10),
+            Text(text, style: TextStyle(color: Colors.white, fontSize: 20)),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+class CardButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final IconData icon;
+  final ThemeData theme;
+
+  const CardButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+    required this.icon,
+    required this.theme,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SizedBox(
+      width: double.infinity,
+      height: size.height / 4,
+      child: Card(
+        color: theme.colorScheme.primary,
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        clipBehavior: Clip.antiAlias, // needed for InkWell ripple
+        child: InkWell(
+          onTap: onTap,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(icon, color: Colors.white, size: 50),
+                Text(title, style: TextStyle(color: Colors.white, fontSize: 30)),
+              ],
+            ),
+          ),
         ),
       ),
     );
